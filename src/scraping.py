c1da310f770523e14753198cbb33853c76edffbc
a5822125 <a5822125@aoyama.jp>
2024-12-28 20:52:00 +0900

    fix

--- src/scraping.py
+++ src/scraping.py
@@ -2,6 +2,7 @@ import re  # 正規表現
 import time
 import traceback
 from urllib.request import Request, urlopen
+from pathlib import Path
 
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -11,6 +12,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from tqdm.notebook import tqdm
 from webdriver_manager.chrome import ChromeDriverManager
+
+HTML_RACE_DIR = Path("..", "data", "html", "race")
 
 
 def scrape_kaisai_date(from_: str, to_: str) -> list[str]:
@@ -71,3 +74,30 @@ def scrape_race_id_list(kaisai_date_list: list[str]) -> list[str]:
                 print(traceback.format_exc())  # エラー把握
                 break
     return race_id_list
+
+
+def scrape_html_race(race_id_list: list[str], save_dir: Path = HTML_RACE_DIR) -> list[Path]:
+    """
+    netkeiba.comのraceページのhtmlをスクレイピングして, save_dirに保存する関数.
+    すでにhtmlが存在する場合はスキップされて, 新たに取得されたhtmlのパスだけが返ってくる.
+    """
+    html_path_list = []
+    save_dir.mkdir(parents=True, exist_ok=True)
+    for race_id in tqdm(race_id_list):
+        # 外で定義した変数を関数内で直接使用しないで, 引数に渡すことを心掛ける.
+        filepath = save_dir / f"{race_id}.bin"
+        # binファイルがすでに存在する場合はスキップする.
+        if filepath.is_file():
+            print(f"skipped: {race_id}")
+        else:
+            url = f"https://db.netkeiba.com/race/{race_id}"
+            headers = {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"}
+            req = Request(url, headers=headers)
+            html = urlopen(req).read()
+            time.sleep(1)
+            pd.read_html(html)[0]
+            with open(filepath, "wb") as f:
+                f.write(html)
+            html_path_list.append(filepath)
+    return html_path_list
