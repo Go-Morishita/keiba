a59d9a6d886ece714089fcdca2b804a4b0e3eccf
a5822125 <a5822125@aoyama.jp>
2025-01-02 18:04:49 +0900

    add process_horse_results().

--- v3_0_0/src/preprocessing.py
+++ v3_0_0/src/preprocessing.py
@@ -10,8 +10,17 @@ MAPPING_DIR = COMMON_DATA_DIR / "mapping"
 OUTPUT_DIR = Path("..", "data", "01_preprocessed")
 OUTPUT_DIR.mkdir(exist_ok=True, parents=True)
 
-with open(MAPPING_DIR / "sex.json", "r") as f:
+with open(MAPPING_DIR / "sex.json", "r", encoding="utf-8") as f:
     sex_mapping = json.load(f)
+
+with open(MAPPING_DIR / "race_type.json", "r", encoding="utf-8") as f:
+    race_type_mapping = json.load(f)
+
+with open(MAPPING_DIR / "weather.json", "r", encoding="utf-8") as f:
+    weather_mapping = json.load(f)
+
+with open(MAPPING_DIR / "ground_state.json", "r", encoding="utf-8") as f:
+    ground_state_mapping = json.load(f)
 
 
 def process_results(
@@ -34,7 +43,7 @@ def process_results(
     df["weight"] = pd.to_numeric(df["weight"], errors="coerce")
     df["weight_diff"] = df["馬体重"].str.extract(r"\((.+)\)").astype(int)
     df["weight_diff"] = pd.to_numeric(df["weight_diff"], errors="coerce")
-    df["tansho_odds"] = pd.to_numeric(df["単勝"], errors="coerce")  # 変更有
+    df["tansho_odds"] = pd.to_numeric(df["単勝"], errors="coerce")
     df["popularity"] = df["人気"].astype(int)
     df["impost"] = df["斤量"].astype(float)
     df["wakuban"] = df["枠番"].astype(int)
@@ -66,4 +75,43 @@ def process_results(
 
     output_dir.mkdir(parents=True, exist_ok=True)
     df.to_csv(output_dir / save_filename, sep="\t")
+    return df
+
+
+def process_horse_results(
+    input_dir: Path = INPUT_DIR,
+    output_dir: Path = OUTPUT_DIR,
+    save_filename: str = "horse_results.csv",
+    race_type_mapping: dict = race_type_mapping,
+    weather_mapping: dict = weather_mapping,
+    ground_state_mapping: dict = ground_state_mapping,
+    # race_class_mapping: dict = race_class_mapping
+):
+    df = pd.read_csv(input_dir / save_filename, sep="\t")
+    df["rank"] = pd.to_numeric(df["着順"], errors="coerce")
+    df.dropna(subset=["rank"], inplace=True)
+    df["date"] = pd.to_datetime(df["日付"])
+    df["weather"] = df["天気"].map(weather_mapping)
+    df["race_type"] = df["距離"].str[0].map(race_type_mapping)
+    df["course_len"] = df["距離"].str.extract(r"(\d+)").astype(int)
+    df["ground_state"] = df["馬場"].map(ground_state_mapping)
+    df["rank_diff"] = df["着差"].map(lambda x: 0 if x < 0 else x)
+    df["prize"] = df["賞金"].fillna(0)
+    # regex_race_class = "|".join(race_class_mapping.keys())
+    # df["race_class"] = df["レースクラス"].str.extract(rf"({regex_race_class})")[0].map(race_class_mapping)
+    df.rename(columns={"頭数": "n_horses"}, inplace=True)
+
+    df = df[[
+        "horse_id",
+        "date",
+        "rank",
+        "prize",
+        "rank_diff",
+        "weather",
+        "race_type",
+        "course_len",
+        "ground_state",
+        # "race_class",
+        "n_horses"
+    ]]
     return df
